0fd14be1a0eb651413d0b8b8e37cf0e13f04cd05
soamtripathy <soumyaranjantripathy44@gmail.com>
2024-05-07 00:57:28 +0530

    UI completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import gptLogo from "./assets/chatgpt.svg";
+import addBtn from "./assets/add-30.png";
+import msgIcon from "./assets/message.svg";
+import home from "./assets/home.svg";
+import saved from "./assets/bookmark.svg";
+import rocket from "./assets/rocket.svg";
+import sendBtn from "./assets/send.svg";
+import userIcon from "./assets/user-icon.png";
+import gptImgLogo from "./assets/chatgptLogo.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <div className="sideBar">
+          <div className="upperSide">
+            <div className="upperSideTop">
+              <img src={gptLogo} alt="Logo" className="logo" />
+              <span className="brand">ChatGPT</span>
+            </div>
+            <button className="midBtn">
+              <img src={addBtn} alt="new chat" className="addBtn" />
+              New Chat
+            </button>
+            <div className="upperSideBottom">
+              <button className="query">
+                <img src={msgIcon} alt="Query" className="" />
+                What is Programming?
+              </button>
+              <button className="query">
+                <img src={msgIcon} alt="Query" className="" />
+                How to use an API?
+              </button>
+            </div>
+          </div>
+          <div className="lowerSide">
+            <div className="listItems">
+              <img src={home} alt="Home" className="listItemsImg" />
+              Home
+            </div>
+            <div className="listItems">
+              <img src={saved} alt="Saved" className="listItemsImg" />
+              Saved
+            </div>
+            <div className="listItems">
+              <img src={rocket} alt="Upgrade" className="listItemsImg" />
+              Upgrade to Pro
+            </div>
+          </div>
+        </div>
+        <div className="main">
+          <div className="chats">
+            <div className="chat">
+              <img className="chatImg" src={userIcon} alt="" />
+              <p className="txt">
+                Lorem className
+="" ipsum dolor sit amet consectetur adipisicing elit. Est
+                veritatis repellendus aliquam quas reprehenderit, excepturi
+                voluptatem nulla quo voluptatum nemo.
+              </p>
+            </div>
+            <div className="chat bot">
+              <img className="chatImg" src={gptImgLogo} alt="" />
+              <p className="txt">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab,
+                excepturi voluptatum! Accusantium asperiores aut nisi eos
+                dolores magni veniam debitis et unde obcaecati? Placeat laborum
+                nostrum eaque voluptates delectus mollitia recusandae non beatae
+                porro ab quos, aliquam magnam soluta neque ex cumque vitae
+                quaerat enim. Dicta doloremque quos nemo molestias distinctio
+                corrupti error impedit, sunt inventore laborum vitae harum iusto
+                iure natus voluptatem similique, earum adipisci ipsum
+                perspiciatis voluptatum incidunt et omnis! Quae et qui
+                perferendis, molestiae maxime aliquid quos earum vel totam?
+                Distinctio voluptatem ea fugit voluptates, tenetur ipsam harum
+                officia, tempora dolor tempore blanditiis velit perspiciatis
+                incidunt amet?
+              </p>
+            </div>
+          </div>
+          <div className="chatFooter">
+            <div className="inp">
+              <input type="text" placeholder="Send a message" />
+              <button className="send">
+                <img src={sendBtn} alt="send" />
+              </button>
+            </div>
+            <p>
+              ChatGPT can make mistakes. Consider checking important
+              information.
+            </p>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
